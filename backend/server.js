196217adef0dc03a3a0bf37418c3c42957f67c9a
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-10-11 13:52:52 -0400

    Connect MongoDB

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,12 @@
 // es6 in NodeJS
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+connectDB()
 
 const app = express()
 
@@ -19,6 +22,6 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find(p => p._id === req.params.id)
   res.json(product)}) 
 
-const PORT = process.env.PORT || 5000 
+const PORT = process.env.PORT || 5000  
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)) 
