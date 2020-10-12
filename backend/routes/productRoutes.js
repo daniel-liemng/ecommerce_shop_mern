ce8a16d7e04b04d79d811856d8405c4de4c7466d
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-10-12 04:24:48 -0400

    Redux HomeScreen

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,37 @@
-import express from 'express'
-import Product from '../models/productModel.js'
-import asyncHandler from 'express-async-handler'
+import express from "express";
+import Product from "../models/productModel.js";
+import asyncHandler from "express-async-handler";
 
-const router = express.Router()
+const router = express.Router();
 
 // @desc    Fetch all products
 // @route   GET  /api/products
 // @access  Public
-router.get('/', asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
 
-  res.json(products)
-}))
+    res.json(products);
+  })
+);
 
 // @desc    Fetch single product
 // @route   GET  /api/products/:id
 // @access  Public
-router.get('/:id', asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id)
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
-  if (product) {
-    res.json(product)
-  } else {
-    // res.status(404).json({ message: 'Product Not Found' })
-    res.status(404)
-    throw new Error('PRODUCT Not Found')
-  }  
-})) 
+    if (product) {
+      res.json(product);
+    } else {
+      // res.status(404).json({ message: 'Product Not Found' })
+      res.status(404);
+      throw new Error("PRODUCT Not Found");
+    }
+  })
+);
 
-export default router
+export default router;
