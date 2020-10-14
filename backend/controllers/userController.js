cf4b408b6d036b29f34ae9f62b046b7fafefafb9
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-10-13 23:05:38 -0400

    Generate JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 
 // @desc    Auth user and get token
 // @route   POST  /api/users/login
@@ -10,12 +11,12 @@ export const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    res.json({
+    return res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
