48a2d226765ec57806fc429cc0bb96d0bef942dd
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-10-14 23:22:24 -0400

    User Registration and Password Encrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -25,9 +25,21 @@ const userSchema = mongoose.Schema(
   { timestamps: true }
 );
 
+//// For LOGIN
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+//// For REGISTER
+userSchema.pre("save", async function (next) {
+  // Run this "SAVE" only when updating the password
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
