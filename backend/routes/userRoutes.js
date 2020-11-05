0ec580663cdaa36b405c96bda6a621d08d84deb8
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-11-05 11:18:30 -0500

    Admin Middleware and getUsers api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,12 +6,12 @@ import {
   updateUserProfile,
   getUsers,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // router.route("/login").post(authUser);
-router.route("/").post(registerUser).get(protect, getUsers);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 router
   .route("/profile")
