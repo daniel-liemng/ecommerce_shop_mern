a7759917a800eb02a6ab2c91feebc3e2d2a1fd3f
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-11-06 02:52:13 -0500

    Admin Update Product - Fix

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -6,7 +6,10 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import FormContainer from "../components/FormContainer";
 import { listProductDetails, updateProduct } from "../actions/productActions";
-import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
+import {
+  PRODUCT_UPDATE_RESET,
+  PRODUCT_DETAILS_RESET,
+} from "../constants/productConstants";
 
 const ProductEditScreen = ({ match, history }) => {
   const productId = match.params.id;
@@ -36,6 +39,7 @@ const ProductEditScreen = ({ match, history }) => {
     // Update OK -> reset, redirect
     if (successUpdate) {
       dispatch({ type: PRODUCT_UPDATE_RESET });
+      dispatch({ type: PRODUCT_DETAILS_RESET });
 
       history.push("/admin/productlist");
     } else {
